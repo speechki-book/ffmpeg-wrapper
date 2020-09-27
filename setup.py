85b6ddf2131cd806f97cf73a19971f5a0920886c
Pavel Maltsev <zeden123@gmail.com>
2020-09-27 16:27:09 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
         "Python",
         "Wrapper",
     ],  # Keywords that define your package best
-    install_requires=[],  # I get to this in a second
     classifiers=[
         "Development Status :: 4 - Beta",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         "Intended Audience :: Developers",  # Define that your audience are developers
