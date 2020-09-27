74436b24bab1654d1d917c2be7d6beeca99e0952
Pavel Maltsev <zeden123@gmail.com>
2020-09-27 16:29:52 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -21,6 +21,9 @@ setup(
         "Python",
         "Wrapper",
     ],  # Keywords that define your package best
+    install_requires=[
+        "pydantic",
+    ],  # I get to this in a second
     classifiers=[
         "Development Status :: 4 - Beta",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         "Intended Audience :: Developers",  # Define that your audience are developers
